5da209e453f5d0db0e100e92ac27e8f653dc8d0c
Prithvirajkumar <prithvirajkumar14@gmail.com>
2023-08-15 17:15:49 -0400

    commit second test

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ Sentry.init({
   //   }
   //   return event;
   // },
-  // Commit test
 });
 
 (function () {
--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ GITHUB_REPO="node-playground"
 # sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
 
 # sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
-sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits "$VERSION" --commit "Prithvirajkumar/node-playground@deadbeef"
+sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits "$VERSION" --commit "Prithvirajkumar/node-playground@3c759a33a956224bd2a68fbf3809fc7fc88835b0"
 
 node index.js
