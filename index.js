bdda96e1def32b47dab902990b379b1919725ccf
Prithvirajkumar <prithvirajkumar14@gmail.com>
2023-08-15 17:07:44 -0400

    commit test

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ Sentry.init({
   //   }
   //   return event;
   // },
-  // test
 });
 
 (function () {
--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 SENTRY_ORG=prithvi-0c
-SENTRY_PROJECT=prithvi-node
-RELEASE=test
+SENTRY_PROJECT=nodejs
+RELEASE="my-repo"
+VERSION=`sentry-cli releases propose-version`
+GITHUB_ORG="https://github.com/Prithvirajkumar"
+GITHUB_REPO="node-playground"
+
 
 # sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
 # sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
 
-sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --commit $RELEASE
+# sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
+sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits "$VERSION" --commit "Prithvirajkumar/node-playground@deadbeef"
 
 node index.js
