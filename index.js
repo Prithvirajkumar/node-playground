76b11a5ee2fc52e369df65fcc836899220dfda54
Prithvirajkumar <prithvirajkumar14@gmail.com>
2024-11-12 14:49:32 -0500

    new added code

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
 const Sentry = require("@sentry/node");
-const Tracing = require("@sentry/tracing");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
-  // dsn: "https://2687e6d7c6af4b14838b2651a40ad5a3@o1161257.ingest.sentry.io/6307748",
-  // dsn: "https://82a71827fb8643dea9be8f45fe0c97b5@o1161257.ingest.sentry.io/4504644936859648",
-  dsn: "https://53cea17c1e5940eb928a215cacad298d@o87286.ingest.sentry.io/6469184",
-  tracesSampleRate: 1,
-  integrations: [new Sentry.Integrations.Http({ tracing: true })],
-  // beforeSendTransaction(event) {
-  //   if (event.tags && event.tags["url"] === "https://sentry.io/") {
-  //     event.environment = "sentry-environment";
-  //     event.release = "sentry-release";
-  //   }
-  //   return event;
-  // },
-  // test 6
-  release: "slack-test-release-2",
+  dsn: "https://2687e6d7c6af4b14838b2651a40ad5a3@o1161257.ingest.us.sentry.io/6307748",
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
 });
 
-// (function () {
-//   const transaction = Sentry.startTransaction({
-//     op: "Custom Transaction",
-//     name: "Custom Transaction",
-//   });
-//   const span = transaction.startChild({
-//     op: "spanOp",
-//     description: `spanDescription`,
-//   });
-//   Sentry.setTag("url", "https://sentry.io/");
-//   span.finish();
-//   transaction.finish();
-//   throw new Error("Help");
-// })();
+function problematicFunction() {
+  throw new Error("This is a test error for Sentry.");
+}
 
-(function () {
-  Sentry.configureScope(function (scope) {
-    scope.setLevel("fatal");
-    Sentry.setTag("se", "Prithvi");
-    throw new Error("Slack Error 2");
-  });
-})();
+try {
+  problematicFunction();
+} catch (error) {
+  Sentry.captureException(error);
+  console.error("An error occurred:", error);
+}
